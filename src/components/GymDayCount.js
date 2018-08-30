2dc8c48c2a7d9fd8eafff1a6f6ea1584269bf014
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-08-30 12:45:47 -0400

    switched to stateless components

--- src/components/GymDayCount.js
+++ src/components/GymDayCount.js
@@ -1,37 +1,72 @@
 import React, { Component } from 'react'
 
-class GymDayCount extends Component {
-
-    percentToDecimal(decimal) {
-        return(
-            (decimal * 100) + '%'
-        )
-    } 
-
-    calcGoalProgress(total, goal) {
-        return (
-            this.percentToDecimal(total/goal)
-        )
-    }
-
-    render() {
-        return(
-            <div className="gym-day-count">
-                <div className="total-days">
-                    <span>{this.props.total} days</span>
-                </div>
-                <div className="lifting-days">
-                    <span>{this.props.lifting} days</span>
-                </div>
-                <div className="cardio-days">
-                    <span>{this.props.cardio} days</span>
-                </div>
-                <span>
-                    {this.calcGoalProgress(this.props.total, this.props.goal)}
-                </span>
-            </div>
-        )
-    }
+const percentToDecimal = (decimal) => {
+    return(
+        (decimal * 100) + '%'
+    ) 
 }
+
+const calcGoalProgress = (total, goal) => {
+    return(
+        percentToDecimal(total/goal)
+    )
+}
+    
+const GymDayCount = ({total, lifting, cardio, goal}) => (
+    <div className="gym-day-count">
+        <div className="total-days">
+            <span>{total} days</span>
+        </div>
+        <div className="lifting-days">
+            <span>{lifting} days</span>
+        </div>
+        <div className="cardio-days">
+            <span>{cardio} days</span>
+        </div>
+        <span>
+            {calcGoalProgress(total, goal)}
+        </span>
+    </div>
+
+)
+
+
+
+// Same as above but written with ES6 classes
+
+
+// class GymDayCount extends Component {
+
+//     percentToDecimal(decimal) {
+//         return(
+//             (decimal * 100) + '%'
+//         )
+//     } 
+
+//     calcGoalProgress(total, goal) {
+//         return (
+//             this.percentToDecimal(total/goal)
+//         )
+//     }
+
+//     render() {
+//         return(
+//             <div className="gym-day-count">
+//                 <div className="total-days">
+//                     <span>{this.props.total} days</span>
+//                 </div>
+//                 <div className="lifting-days">
+//                     <span>{this.props.lifting} days</span>
+//                 </div>
+//                 <div className="cardio-days">
+//                     <span>{this.props.cardio} days</span>
+//                 </div>
+//                 <span>
+//                     {this.calcGoalProgress(this.props.total, this.props.goal)}
+//                 </span>
+//             </div>
+//         )
+//     }
+// }
 
 export default GymDayCount
